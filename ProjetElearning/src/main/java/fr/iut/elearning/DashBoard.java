169e887da1a56d0e6e8f2c2e1f649ebb425742f9
hussam <chaudhry.hussam@gmail.com>
2014-06-28 20:25:18 +0200

    Added a test example with JUnit and Mockito

--- ProjetElearning/src/main/java/fr/iut/elearning/DashBoard.java
+++ ProjetElearning/src/main/java/fr/iut/elearning/DashBoard.java
@@ -2,7 +2,6 @@ package fr.iut.elearning;
 
 import java.util.Locale;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -11,11 +10,14 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import fr.iut.elearning.model.SessionBean;
 import fr.iut.elearning.model.Status;
+import fr.iut.elearning.model.Teacher;
+import fr.iut.elearning.service.TeacherService;
 
 /**
  * Handles requests for the professor EDT page.
@@ -29,6 +31,14 @@ public class DashBoard {
 	@Autowired
     public SessionBean sessionBean;
 	
+	@Autowired
+	private TeacherService teacherService;
+	
+	@Autowired
+	public DashBoard(TeacherService teacherService) {
+		this.teacherService = teacherService;
+	}
+
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
@@ -44,6 +54,16 @@ public class DashBoard {
 		Status statusSession = sessionBean.getStatus();
 		return statusSession.equals(Status.Etudiant) ? "DashBoard" : "profDashBoard";
 	}
-	
-	
+
+	@RequestMapping(value = "/getTeacher/{id}", method = RequestMethod.GET)
+	public String getTeacherById(@PathVariable int id, Model model) {
+//		session.setAttribute("sessionBean", sessionBean);
+		
+//		if(!Login.VerificationAccesPage(sessionBean, statusController))
+//			return "NonAutorise";
+		
+		model.addAttribute("teacherData", teacherService.findById(id));
+		
+		return "DashBoard";
+	}		
 }
